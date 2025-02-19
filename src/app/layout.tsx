3f8f812906ed5833cab8c040cae3f8fa59633052
santosh <santosh@mmag.in>
2025-02-19 16:00:13 +0530

    app.js delete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./main.css";
-// import "./";
-// import "./app.js";
 
 
 const geistSans = Geist({
